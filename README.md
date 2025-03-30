dddfb374aa639ad373520f01f8c905bad107ee1a
Audyari WIyono <Audy123ari@gmail.com>
2025-03-30 11:54:32 +0700

    String Resource

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-"# Liburan-Belajar-Android" 
-"# Liburan-Belajar-Android" 
-"# Liburan-Belajar-Android" 
-"# Liburan-Belajar-Android" 
+# BelajarAndroidDasar
+
+1. Manifest File :
+    - berisikan informasi dari aplikasi yang kita buat, seperti informasi Activity, Permission, Intent, Provider, Receiver, Service, dan lain-lain
+
+2. Activity adalah main function dari aplikasi android
+    ada di : AndroidManifest.xml
+
+3. Layout : yg perlu di perhatikan :
+    - layout linear layout
+    - activity
+
+4. penggunaan View di android :
+    - menggunakan  xml :
+    - textview
+    - edittext
+    - button
+
+
+5. pembuatan View ID
+
+    Pembuatan nya di XML ya : 
+    -  android:id="@+id/nameText"
+
+6. Action Listener adalah object yang bisa kita tambahkan ke komponen ketika sebuah aksi dilakukan ke komponen tersebut
+
+    contoh nya pada Button
+
+7. Lateinit adalah pengembangan dari view ID dan indViewById biar lebih mudah mengelola nya
+
+8. Log cara pembuatan :
+    - VERBOSE
+    - DEBUG
+    - INFO
+    - WARN
+    - ERROR
+
+9. Resource :
+  - text
+  - gambar
+  - animasi
+
+10. String Resource :
+    - Formatting String :  <string name="sayHelloTextView">Hello %1$s </string>
+    - String Array : 
--- app/src/main/java/butocakil/belajarandroiddasar/MainActivity.kt
+++ app/src/main/java/butocakil/belajarandroiddasar/MainActivity.kt
@@ -1,24 +1,51 @@
 package butocakil.belajarandroiddasar
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var sayHelloButton: Button
+    private lateinit var sayHelloTextView: TextView
+    private lateinit var sayHelloTextView2: TextView
+
+    
+    private fun initComponent() {
+        nameEditText = findViewById(R.id.nameEditText)
+        sayHelloButton = findViewById(R.id.sayHelloButton)
+        sayHelloTextView = findViewById(R.id.sayHelloTextView)
+        sayHelloTextView2 = findViewById(R.id.sayHelloTextView2)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hello_word)
 
+        initComponent()
 
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val sayHelloButton = findViewById<Button>(R.id.sayHelloButton)
-        val sayHelloTextView = findViewById<TextView>(R.id.sayHelloTextView)
-
+        sayHelloTextView2.text = resources.getString(R.string.app_name)
+     
         sayHelloButton.setOnClickListener {
+            // Log.v("log di sayHelloButton verbose", "Button diklik")
+            // Log.d("log di sayHelloButtonc debug", "Button diklik")
+            // Log.i("log di sayHelloButton info", "Button diklik")
+            // Log.w("log di sayHelloButton warning", "Button diklik")
+            // Log.e("log di sayHelloButton error", "Button diklik")
+           
+            // Get name from EditText
             val name = nameEditText.text.toString()
-            sayHelloTextView.text = "Hello, $name!"
+            sayHelloTextView.text = resources.getString(R.string.sayHelloTextView, name)
+            // Log.d("log di sayHelloButton", "Hello, $name!")
+
+            // Mengambil String Array Resource
+            resources.getStringArray(R.array.names).forEach {
+                Log.d("log di sayHelloButton", "Name: $it")
+            }
         }
     }
 }
